fce4449f88104ada2324ecc5e4303c89ed8c98fc
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-11-19 12:53:19 +0200

    Add Services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import HeroBanner from "./components/hero-banner/HeroBanner";
 import Footer from "./components/footer/Footer";
 import BackToTopButton from "./components/back-to-top-button/BackToTopButton";
 import AboutUs from "./components/about-us/AboutUs";
+import Services from "./components/services/Services";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
      <Navigation />
      <HeroBanner />
      <AboutUs />
+     <Services />
      <Footer />
      <BackToTopButton />
     </>
