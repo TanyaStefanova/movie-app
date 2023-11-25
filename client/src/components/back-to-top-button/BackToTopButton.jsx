bebdf52bbed7e8b5786b6af6336e073113f56fc2
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-11-25 20:44:34 +0200

    Clear client folder

--- client/src/components/back-to-top-button/BackToTopButton.jsx
+++
@@ -1,5 +0,0 @@
-export default function BackToTopButton(){
-    return(
-        <a href="#" className="btn btn-lg btn-primary btn-lg-square back-to-top"><i className="bi bi-arrow-up"></i></a>
-    );
-}
