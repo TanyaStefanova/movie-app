3f17ea3034ce5e509efde67942162610a5bcb4be
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-11-26 23:39:57 +0200

    Add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Movies from './components/movies/Movies';
@@ -9,8 +12,13 @@ function App() {
   return (
     <>
       <Header />
+
       <Home />
       <Movies />
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+        {/* <Route path='/movies' element={<Movies />} /> */}
+      </Routes>
     </>
   )
 }
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,9 +8,9 @@ export default function Header() {
     return(
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Filmster</Navbar.Brand>
+          <Link to="/">Filmster</Link>
           <Nav>
-            <Nav.Link href="#home">Movies</Nav.Link>
+            <Link to="/movies">Movies</Link>
             <Nav.Link href="#features">Add Movie</Nav.Link>
             <Nav.Link href="#pricing">Login</Nav.Link>
             <Nav.Link href="#pricing">Register</Nav.Link>
