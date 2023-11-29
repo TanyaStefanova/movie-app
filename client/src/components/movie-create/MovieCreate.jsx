827537fa4f24eff3af235ed0383b5b2a44e7644d
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-11-29 02:07:31 +0200

    Make movieCreate controlled form

--- client/src/components/movie-create/MovieCreate.jsx
+++ client/src/components/movie-create/MovieCreate.jsx
@@ -1,50 +1,133 @@
+import { useState } from "react";
+
+import * as movieService from "../../services/movieService";
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
+const FORM_KEYS = {
+    title: 'title',
+    year: 'year',
+    posterUrl: 'posterUrl',
+    type: 'type',
+    genres: 'genres',
+    plot: 'plot',
+}
+
+const formInitialState = {
+    [FORM_KEYS.title]: '',
+    [FORM_KEYS.year]: '',
+    [FORM_KEYS.posterUrl]: '',
+    [FORM_KEYS.type]: 'Movie',
+    [FORM_KEYS.genres]: 'Crime',
+    [FORM_KEYS.plot]: '',
+}
+
+
 export default function MovieCreate({ show, onClose }) {
+    const [formValues, setFormValues] = useState(formInitialState);
+
+    
+    const changeHandler = (e) => {
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+        let value = e.target.value;
+
+        if(e.target.type === 'number'){
+            value = Number(e.target.value);
+        }
+
+        setFormValues(state => ({
+            ...state,
+            [e.target.name]: value,
+        }));
+    }
+
+    const resetFormHandler = (e) => {
+        setFormValues(formInitialState);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        resetFormHandler();
+
+        movieService.create(formValues)
+        console.log(formValues);
+    }
+
 
     return (
         <>
-
             <Modal show={show} onHide={onClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>Add Movie</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form>
+                    <Form onSubmit={submitHandler}>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Title</Form.Label>
+                        {/* <Form.Group className="mb-3"> */}
+                            <Form.Label >Title</Form.Label>
                             <Form.Control
                                 type="text"
+                                name={FORM_KEYS.title}
+                                // id="title"
+                                value={formValues.title}
+                                onChange={changeHandler}
                                 placeholder="Fast and Furious"
                                 autoFocus
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                            <Form.Label>Year</Form.Label>
+                        {/* <Form.Group className="mb-3"> */}
+                            <Form.Label >Year</Form.Label>
                             <Form.Control
                                 type="number"
+                                name={FORM_KEYS.year}
+                                // id="year"
+                                value={formValues.year}
+                                onChange={changeHandler}
                                 placeholder="1974"
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                            <Form.Label>Poster Url</Form.Label>
+                        {/* <Form.Group className="mb-3" > */}
+                            <Form.Label >Poster Url</Form.Label>
                             <Form.Control
                                 type="text"
+                                name={FORM_KEYS.posterUrl}
+                                // id="posterUrl"
+                                value={formValues.posterUrl}
+                                onChange={changeHandler}
                                 placeholder="https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg"
                             />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                            <Form.Select aria-label="Default select example">
-                                <option>Types</option>
+                        {/* <Form.Group className="mb-3"> */}
+                            <Form.Select
+                                aria-label="type"
+                                placeholder="Type"
+                                name={FORM_KEYS.type}
+                                // id="type"
+                                value={formValues.type}
+                                onChange={changeHandler}
+                            >
+                                {/* <option>Type</option> */}
                                 <option value="movie">Movie</option>
                                 <option value="tv-show">TV Show</option>
                             </Form.Select>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                            <Form.Select aria-label="Default select example">
-                                <option>Genres</option>
+                        {/* <Form.Group className="mb-3"> */}
+                            <Form.Select
+                                aria-label="genres"
+                                placeholder="Genres"
+                                name={FORM_KEYS.genres}
+                                // id="genres"
+                                value={formValues.genres}
+                                onChange={changeHandler}
+                            >
+                                {/* Genres */}
                                 <option value="crime">Crime</option>
                                 <option value="drama">Drama</option>
                                 <option value="adventure">Adventure</option>
@@ -53,18 +136,24 @@ export default function MovieCreate({ show, onClose }) {
                                 <option value="thriller">Thriller</option>
                             </Form.Select>
                         </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="exampleForm.ControlTextarea1"
-                        >
-                            <Form.Label>Plot</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        {/* <Form.Group className="mb-3"> */}
+                            <Form.Label >Plot</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                rows={3}
+                                type="text"
+                                name={FORM_KEYS.plot}
+                                // id="plot"
+                                value={formValues.plot}
+                                onChange={changeHandler}
+                            />
                         </Form.Group>
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="primary" type="submit" onClick={onClose}>
-                        Add Movie
+                    <Button variant="primary" type="submit" onClick={submitHandler}>
+                        Add
                     </Button>
                     <Button variant="secondary" onClick={onClose}>
                         Close
--- client/src/services/movieService.js
+++ client/src/services/movieService.js
@@ -1,6 +1,7 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/data/movies'
+// const baseUrl = 'http://localhost:3030/data/movies'
+const baseUrl = 'http://localhost:3030/jsonstore/movies'
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
