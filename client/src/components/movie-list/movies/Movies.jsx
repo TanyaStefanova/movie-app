870ef1d10f15845e2addbda04620e4722089b4ed
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-12-02 13:03:18 +0200

    Add stylization

--- client/src/components/movie-list/movies/Movies.jsx
+++ client/src/components/movie-list/movies/Movies.jsx
@@ -1,17 +1,34 @@
+import styles from './Movies.module.css'
+import AddFavourites from '../../add-favourites/AddFavourites';
+
+
 export default function Movies({ movies }) {
     return (
         <>
             <h3>Movies</h3>
-            <div className='container-fluid'>
-                <div className="row" style={{ flexWrap: 'nowrap', overflowX: 'auto' }}>
+            <div className={styles.containerFluid}>
+
+                {/* {<div style={{ fontSize: "30px", color: 'white'}}>{" < "}</div>} */}
+                {/* <button><div>&#8249;</div></button> */}
+                <div className={styles.row}>
+
+
                     {movies.map(movie => (
-                        <img
-                            key={movie._id}
-                            src={movie.posterUrl}
-                            style={{ width: '15%' }}
-                            alt={movie.name} />
+                        <div className={styles.imageContainer}>
+                            <img
+                                key={movie._id}
+                                src={movie.posterUrl}
+                                className={styles.rowPoster}
+                                alt={movie.name}
+                                style={{ width: '11em', height: '100%' }} />
+                            <div className={`${styles.overlay} d-flex align-items-center justify-content-center`}><AddFavourites /></div>
+                        </div>
                     ))}
+
+
+                    {/* {<div style={{ fontSize: "30px", color: 'white' }}>{" > "}</div>} */}
                 </div>
+                {/* <button><div>&#8250;</div></button> */}
             </div>
         </>
     );
--- client/src/components/movie-list/tv-shows/TvShows.jsx
+++ client/src/components/movie-list/tv-shows/TvShows.jsx
@@ -1,12 +1,26 @@
-export default function TvShows({movies}){
+import styles from './TvShows.module.css'
+import AddFavourites from '../../add-favourites/AddFavourites';
+
+export default function TvShows({ movies }) {
     return (
         <>
-           <h3>TV Shows</h3>
-            <div className='container-fluid'>
-                <div className="row" style={{ flexWrap: 'nowrap', overflowX: 'auto' }}>
+            <h3>TV Shows</h3>
+            <div className={styles.containerFluid}>
+
+                <div className={styles.row}>
+
                     {movies.map(movie => (
-                        <img key={movie._id} src={movie.posterUrl} style={{ width: '15%' }} />
+                        <div className={styles.imageContainer}>
+                            <img
+                                key={movie._id}
+                                src={movie.posterUrl}
+                                className={styles.rowPoster}
+                                alt={movie.name}
+                                style={{ width: '11em', height: '100%' }} />
+                            <div className={`${styles.overlay} d-flex align-items-center justify-content-center`}><AddFavourites /></div>
+                        </div>
                     ))}
+
                 </div>
             </div>
         </>
