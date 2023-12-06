87b40ca0939d7a9fef58a9bbe284b78e107720a8
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-12-07 01:46:21 +0200

    Seed data in server

--- server/server.js
+++ server/server.js
@@ -1569,6 +1569,23 @@
                 isFavouredBy: "35c62d76-8152-4626-8712-eeb96381bea8",
                 _id: "a9bae6d8-793e-46c4-a9db-deb9e3484909as1"
             }
+        },
+        tvshows: {
+            "a9bae6d8-793e-46c4-a9db-deb9e3484909as1": {
+                _ownerId: "35c62d76-8152-4626-8712-eeb96381bea8",
+                title: "Crocodile Dundee",
+                year: "1986",
+                type: "tvShow",
+                genres: [
+                    "Adventure",
+                    "Comedy"
+                ],
+                plot: "An American reporter goes to the Australian outback to meet an eccentric crocodile poacher and invites him to New York City.",
+                posterUrl: "https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_240x360_crop_center.progressive.jpg?v=1573652543",
+                _createdOn: 1614935268135,
+                isFavouredBy: "35c62d76-8152-4626-8712-eeb96381bea8",
+                _id: "a9bae6d8-793e-46c4-a9db-deb9e3484909as1"
+            }
         }
     };
     var rules$1 = {
