7e39dea20f7bbbd6e4d594a476a86781d2cdb208
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2023-12-06 15:26:23 +0200

    Style search page

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -6,15 +6,19 @@ export default function Search({ searchedMovies }) {
         <>
             <h1>Search Results</h1>
             <div className="container">
+                {searchedMovies.length == 0 && (
+                    <h1>No movie with this title</h1>
+                )}
                 {searchedMovies.map(movie => (
-                    <div key={movie._id} className="card v4 tight">
+                    <div key={movie._id} style={{marginTop: '20px', backgroundColor:'#212529'}} className="card v4 tight">
                         <div className="wrapper">
                             <div className={styles.image} >
-                                <div><img style={{width: '94px', height: '141px'}} src={movie.posterUrl} alt="movie-poster" /></div>
-                                        <div>
-                                           <div className="title" ><h2>{movie.title}</h2></div>
-                                        <div className="plot" ><p>{movie.plot}</p></div>   
-                                        </div>
+                                <div style={{float: 'left', width: '10%'}}><img style={{ width: '94px', height: '141px' }} src={movie.posterUrl} alt="movie-poster" />
+                                </div>
+                                <div className={styles.details}>
+                                    <div className={styles.title}><h2>{movie.title}</h2></div>
+                                    <div className={styles.overview}><p>{movie.plot}</p></div>
+                                </div>
                             </div>
                         </div>
                     </div>
