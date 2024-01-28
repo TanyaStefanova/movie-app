792d58c7af6f4a48f26a154a9644187229c70de1
TanyaStefanova <79509493+TanyaStefanova@users.noreply.github.com>
2024-01-28 18:45:35 +0200

    Add setupTests file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,5 @@
 import App from './App';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 test('Always true test', () => {
     expect(true).toBe.true;
